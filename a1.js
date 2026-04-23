cc77c5c23e31217907fd252bfc04fc4cd1b7a018
Mohammad Javid Miakhil <124631494+MohammadJavid07@users.noreply.github.com>
2026-04-23 17:41:34 +0300

    Delete a1.js

--- a1.js
+++
@@ -1,7 +0,0 @@
- 
-const btn = document.getElementById("prompt-btn");
-const output = document.getElementById("output");
-btn.addEventListener("click", () => {
-  const userName = prompt("What is your name?", "");
-  output.textContent = "Hello, " + userName + "!";
-});
