720fdfbc63d6deb6a087c1eca3ac2a3a67f36b25
Mohammad Javid Miakhil <124631494+MohammadJavid07@users.noreply.github.com>
2026-04-24 19:34:02 +0300

    Implement golf score translator function
    Added a golf score translator function to evaluate scores based on par.

--- Practice.js
+++ Practice.js
@@ -107,3 +107,31 @@ const calculateArea = (length, width) => {
 };
 
 console.log(calculateArea(5, 10)); // Output: "The area of the rectangle is 50 square units."
+
+// a Golf score translator game
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par,strokes){
+if(strokes === 1){
+  return "Hole-in-one!";
+}
+else if(strokes <= par-2){
+  return "Eagle";
+}
+else if(strokes === par-1){
+  return "Birdie";
+}
+else if(strokes === par){
+  return "Par";
+}
+else if(strokes === par+1){
+  return "Bogey";
+}
+else if(strokes === par+2){
+  return "Double Bogey";
+}
+else if(strokes >= par+3){
+  return "Go Home!";
+}
+}
+console.log(golfScore(3,1));
